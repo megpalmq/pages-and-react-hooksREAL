09e2ac7be0b8a44e2aef62156e76c31348230451
Megan Palmquist <megpalmq@iu.edu>
2024-09-18 10:31:51 -0400

    publish

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,21 @@ import {useState} from 'react';
 
 export default function Home() {
   //let score= 0;
-  const scoreState= useState();
-  const score = scoreState[0];
+  const [score, setScore]= useState(1);
 
-  console.log(scoreState);
+  //Deconstructing arrays:
+  //1. the variable names used for the items doesnt matter
+
+  console.log(score);
 
   function addToScore(){
     //score++;
+    setScore:(score+1);
     console.log('score');
+  }
+  function doublePoints(){
+    addToScore();
+    addToScore();
   }
 
   return (
@@ -18,6 +25,7 @@ export default function Home() {
       <h1>Home</h1>
       <div>
       <button onClick={addToScore}>Add 1</button>
+      <button onClick={doublePoints}>Add 2</button>
       <p>Score: 0</p>
 
       </div>
