3024a31a112227382ae1f7b4e61e292cc94dbba9
Megan Palmquist <megpalmq@iu.edu>
2024-09-18 10:08:03 -0400

    added button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+"use client";
+
 export default function Home() {
+  let score= 0;
+
+  function addToScore(){
+    score++;
+  }
+
   return (
     <main>
       <h1>Home</h1>
+      <div>
+      <button onClick={addToScore}>Add 1</button>
+      <p>Score: 0</p>
+
+      </div>
     </main>
   );
 }
